b86834ff90c3a51d5f0dfc46529886addcc1ab78
yuta-hamasaki <yh02052002@gmail.com>
2024-10-03 16:02:21 -0700

    Done

--- dist/ex03.js
+++ dist/ex03.js
@@ -1,14 +1,12 @@
+"use strict";
 // Exercise: Write a function `logValues` that takes an array of any type of values (numbers, strings, booleans, etc.).
 // The function should iterate through the array and log each value along with its type.
-
-
-function logValues(values: any[]){
-  for(let i = 0; i < values.length; i++){
-    let type = typeof values[i]
-    console.log(`${values[i]}(${type})`)
-  }
+function logValues(values) {
+    for (let i = 0; i < values.length; i++) {
+        let type = typeof values[i];
+        console.log(`${values[i]}(${type})`);
+    }
 }
-
 logValues([42, "Hello", true, { name: "Alice" }]);
 // Expected output:
 // 42 (number)
--- src/ex02.ts
+++ src/ex02.ts
@@ -1,8 +1,22 @@
 // Exercise: Create an enum `DaysOfWeek` with values for each day of the week.
 // Then, write a function `isWeekend` that accepts a value from the enum and returns `true` if it's Saturday or Sunday, and `false` otherwise.
+enum DaysOfWeek {
+  Sunday,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+}
 
-function isWeekend(day) {
-
+function isWeekend(day:DaysOfWeek) {
+  return day === DaysOfWeek.Saturday || day === DaysOfWeek.Sunday
+  // if(day === DaysOfWeek.Saturday || day === DaysOfWeek.Sunday){
+  //   return true 
+  // }else{
+  //   return false
+  // }
 }
 
 console.log(isWeekend(DaysOfWeek.Saturday));
